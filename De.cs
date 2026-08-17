4fa8f68a46b2489fdbe6ae881e376d255d84bcb5
ThomasBellion-GitHub <thollion65@gmail.com>
2026-08-17 20:35:57 +0200

    Add files via upload

---
+++ De.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Boogle
+{
+    internal class De
+    {
+        private List<char> Faces;    
+        /// <summary>
+        /// "Liste des faces du dé (lettres)."
+        /// </summary>
+        public char LettreVisible { get; private set; } 
+        /// <summary>
+        /// "Lettre visible après le lancer."
+        /// </summary>
+        /// <param name="faces"></param>
+
+        #region Constructeur
+        public De(List<char> faces)
+        {
+            Faces = faces;
+        }
+        #endregion
+
+        #region Méthodes
+        /// <summary>
+        /// "ValeurassociéLettre : On récupère depuis le fichier lettre.txt les deux premières colonnes, soit la lettre est sa valeur."
+        /// </summary>
+        /// <param name="fichier"></param>
+        /// <returns></returns>
+        public static Dictionary<string, int> ValeurAssociéLettre(string fichier)
+        {
+            Dictionary<string, int> dicVal = new Dictionary<string, int>();
+            foreach(var ligne in File.ReadAllLines(fichier))
+            {
+                var parties = ligne.Split(';');
+                string lettre = parties[0];
+                int nmbPoint = int.Parse(parties[1]);
+                dicVal[lettre] = nmbPoint;
+            }
+            return dicVal;
+        }
+        /// <summary>
+        /// "ValeurMot : un mot est composé de lettre. Ainsi, sa valeur est la somme des valeurs des lettres qui le composent.
+        /// On récupère dans <param name="dicVal"></param> les valeurs de chaque lettre et on les somme."
+        /// </summary>
+        /// <param name="mot"></param>
+        /// <returns></returns>
+        public static int ValeurMot(string mot)
+        {
+            Dictionary<string, int> dicVal = ValeurAssociéLettre("Lettres.txt");
+            int motPoint = 0;
+            for (int i = 0; i < mot.Length; i++)
+            {
+                if (dicVal.TryGetValue(mot[i].ToString(), out int valeur))
+                {
+                    motPoint += valeur;
+                }
+                else
+                {
+                    Console.WriteLine(valeur + " non trouvée, utilisez l'alphabet français ou anglais. ");
+                    return 0;
+                }
+            }
+            return motPoint;
+        }
+
+        /// <summary>
+        /// "Créer et lire les probabilités depuis un fichier."
+        /// </summary>
+        /// <param name="fichier"></param>
+        /// <returns></returns>
+        public static Dictionary<string, double> CreerEtLireDepuisFichier(string fichier)
+
+        {
+            Dictionary<string, double> probabilites = new Dictionary<string, double>();
+            int total = 0;
+
+            foreach (var ligne in File.ReadLines(fichier))
+            {
+                var parties = ligne.Split(';');
+                string lettre = parties[0];
+                int nmb = int.Parse(parties[2]);
+
+                total += nmb;
+
+                probabilites[lettre] = nmb;
+            }
+
+            foreach (var lettre in probabilites.Keys)
+            {
+                probabilites[lettre] /= total;
+            }
+
+            return probabilites;
+        }
+
+        /// <summary>
+        /// "Créer un dé aléatoire en fonction des probabilités."
+        /// </summary>
+        /// <param name="probabilites"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static De CreerDeAleatoire(Dictionary<string, double> probabilites)
+        {
+            List<char> faces = new List<char>();
+            Random random = new Random();
+
+            if (probabilites == null || probabilites.Count == 0)
+            {
+                throw new ArgumentException("Les probabilités sont vides ou invalides.");
+            }
+
+            foreach (var entry in probabilites)
+            {
+                int occurrences = (int)(entry.Value * 100);
+
+                
+                if (occurrences > 0)
+                {
+                    for (int i = 0; i < occurrences; i++)
+                    {
+                        faces.Add(entry.Key[0]); 
+                    }
+                }
+            }
+
+            if (faces.Count == 0)
+            {
+                throw new ArgumentException("Aucune face valide n'a été ajoutée au dé. Vérifiez les probabilités.");
+            }
+
+            for (int i = faces.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                char temp = faces[i];
+                faces[i] = faces[j];
+                faces[j] = temp;
+            }
+
+            return new De(faces);  
+        }
+
+        /// <summary>
+        /// "Lancer le dé et tirer une lettre en fonction des faces."
+        /// </summary>
+        public void Lance()
+        {
+            Random random = new Random();
+            int index = random.Next(Faces.Count);
+            LettreVisible = Faces[index]; 
+        }
+
+        /// <summary>
+        /// "Description du dé."
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Face Visible : " + LettreVisible;
+        }
+        #endregion
+    }
+}
---
+++ Joueur.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boogle
+{
+    internal class Joueur
+    {
+        string nom;
+        int score;
+        List<string> mots_trouves = new List<string>();
+
+        #region Constructeurs
+        /// <summary>
+        /// "Création du Constructeur 'Joueur' qui permet de définir un Joueur avec son nom, son score et les mots qu'il a trouvé."
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="score"></param>
+        /// <param name="mots_trouves"></param>
+        public Joueur(string nom, int score, List<string> mots_trouves)
+        {
+            this.nom = nom;
+            this.score = score;
+            this.mots_trouves = new List<string>(mots_trouves); 
+        }
+        #endregion
+
+        #region Propriétés
+        public List<string> Mots_trouves
+        {
+            get { return this.mots_trouves; }
+        }
+        public string Nom
+        {
+            get { return this.nom; }
+        }
+        public int Score
+        {
+            get { return this.score; }
+            set { this.score = value; }
+        }
+        #endregion
+
+        
+        ///<summary> "cherche si un mot à déjà été dit par le joueur" </summary>
+        ///<param name= "mot" > "mot recherché" </param>
+        /// <returns> " un bouléen " </returns>
+        public bool Contain(string mot)
+        {
+            int compteur = 0;
+            for (int i = 0; i < this.mots_trouves.Count; i++)
+            {
+                if (this.mots_trouves[i] == mot)
+                {
+                    compteur++;
+                }
+            }
+            if (compteur == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        ///<summary> "ajoute le mot aux mots déjà trouvés" </summary>
+        ///ajoute au score, la valeur du mot
+        ///<param name= "mot" > "mot à ajouter" </param>
+        public void Add_Mot(string mot)
+        {
+            if (!Contain(mot))
+            {
+                this.mots_trouves.Add(mot);
+                this.score = this.score + De.ValeurMot(mot);
+            }
+        }
+        ///<summary> " Décrit le joueur dans une chaine de caractères" </summary>
+        /// <returns> " Le nom du joueur, son score et les mots qu'il a trouvé" </returns>
+        public string toString()
+        {
+            string res_mots = "";
+            
+            for (int i = 1; i < this.mots_trouves.Count - 1; i++)
+            {
+                if (Contain(this.mots_trouves[i]))
+                {
+                    res_mots += this.mots_trouves[i] + ", "; 
+                }
+            }
+            res_mots = res_mots + this.mots_trouves[this.mots_trouves.Count - 1];
+            return "Nom : " + this.nom + "\n" + "Score : " + this.score + "\n" + "Mots trouves : " + res_mots;
+        }
+
+        
+    }
+}
